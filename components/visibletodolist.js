0888af8a6b0a3ffccd38a4252a9a88dfb0ee4a8a
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-10-11 21:44:28 +0100

    refactor(move todolist out of visibletodolist component):

--- components/visibletodolist.js
+++ components/visibletodolist.js
@@ -3,29 +3,7 @@ import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 import { getVisibleTodos } from '../reducers'
 import { toggleTodo } from './action-creators'
-
-const Todo = ({onClick, completed, text}) => (
-	<li
-		onClick={onClick} style={{
-			textDecoration: completed
-			? 'line-through'
-			: 'none'
-		}}>
-		{text}
-	</li>
-)
-
-const TodoList = ({todos, onTodoClick}) => (
-	<ul>
-		{todos.map(todo =>
-			<Todo
-				key={todo.id}
-				{...todo}
-				onClick={() => onTodoClick(todo.id)}
-			/>
-		)}
-	</ul>
-)
+import TodoList from './todolist';
 
 const mapStateToProps = (state, { params }) => ({
 	todos: getVisibleTodos(state, params.filter || 'all')
