75f85eb430d8707dc29e49d485eed2fb6e1a5bbb
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-10-11 20:15:53 +0100

    feat(introduce react-router):

--- components/footer.js
+++ components/footer.js
@@ -1,63 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { setVisibilityFilter } from './action-creators'
+import FilterLink from './FilterLink'
 
 const Footer = () => (
 	<p>
 		Show: {' '}
 		<FilterLink
-			filter='SHOW_ALL'
+			filter='all'
 		>
 			All
 		</FilterLink>
 		{' '}
 		<FilterLink
-			filter='SHOW_ACTIVE'
+			filter='active'
 		>
 			Active
 		</FilterLink>
 		{' '}
 		<FilterLink
-			filter='SHOW_COMPLETED'
+			filter='completed'
 		>
 			Completed
 		</FilterLink>
 	</p>
 )
-
-const Link = ({active, children, onClick}) => {
-	if (active) {
-		return <span>{children}</span>
-	}
-	return (
-		<a href='#' onClick={e => {
-			e.preventDefault()
-			onClick()
-		}}>
-			{children}
-		</a>
-	)
-}
-
-const mapStateToProps = (
-	state,
-	ownProps
-) => ({
-	active:
-		ownProps.filter ===
-		state.visibilityFilter
-})
-const mapDispatchToProps = (
-	dispatch,
-	ownProps
-) => ({
-	onClick() {
-		dispatch(setVisibilityFilter(ownProps.filter))
-	}
-})
-const  FilterLink = connect(
-	mapStateToProps,
-	mapDispatchToProps
-)(Link)
 
 export default Footer
