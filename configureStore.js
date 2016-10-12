06d1ae758e2233ca4e6596637ebe8a9248934a77
Gaurav Gajjar <gajjargaurav@gmail.com>
2016-10-12 11:02:48 +0100

    refactor(wrapping dispatch to Log Actions):

--- configureStore.js
+++ configureStore.js
@@ -1,16 +1,31 @@
-import todoApp from './reducers'
 import { createStore } from 'redux'
-import { loadState, saveState } from './localStorage'
 import throttle from 'lodash/throttle'
+import todoApp from './reducers'
+import { loadState, saveState } from './localStorage'
 
+const addLoggingToDispatch = (store) => {
+	const rawDispatch = store.dispatch
+	if (!console.group) {
+		return rawDispatch
+	}
+	return (action) => {
+		console.group(action.type)
+		console.log('%c prev state', 'color: gray', store.getState())
+		console.log('%c action', 'color: blue',  action)
+		const retunValue = rawDispatch(action)
+		console.log('%c next state', 'conlor: green', store.getState())
+		console.groupEnd(action.type)
+		return retunValue
+	}
+}
 const configureStore = () => {
 
 	const persistedState = loadState()
+	const store = createStore(todoApp, persistedState)
 
-	const store = createStore(
-		todoApp,
-		persistedState
-	)
+	if (process.env.NODE_ENV !== 'production') {
+		store.dispatch = addLoggingToDispatch(store)
+	}
 
 	store.subscribe(throttle(() => {
 		saveState({
